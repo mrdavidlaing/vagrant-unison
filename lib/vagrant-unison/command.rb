474fe514076d9b7129504a59e637b4cdc815a1bf
David Laing <david@davidlaing.com>
2013-03-28 18:58:29 +0000

    Fix unison command syntax 3

--- lib/vagrant-unison/command.rb
+++ lib/vagrant-unison/command.rb
@@ -26,7 +26,7 @@ module VagrantPlugins
           # Unison over to the guest path using the SSH info
           command = [
             "unison", "-batch",
-            "-ignore='Name {git*,.vagrant/,*.DS_Store}'",
+            "-ignore=Name {git*,.vagrant/,*.DS_Store}",
             "-sshargs", "-p #{ssh_info[:port]} -o StrictHostKeyChecking=no -i #{ssh_info[:private_key_path]}",
             hostpath,
             "ssh://#{ssh_info[:username]}@#{ssh_info[:host]}/#{guestpath}"
--- lib/vagrant-unison/version.rb
+++ lib/vagrant-unison/version.rb
@@ -1,5 +1,5 @@
 module VagrantPlugins
   module Unison
-    VERSION = "0.0.3"
+    VERSION = "0.0.4"
   end
 end
