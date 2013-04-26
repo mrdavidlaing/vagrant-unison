d9da2a0859ae6b881c080a82768f063a6ecc66dd
David Laing <david@davidlaing.com>
2013-04-26 14:08:46 +0100

    Fix bugs with scp upload

--- lib/vagrant-unison/command.rb
+++ lib/vagrant-unison/command.rb
@@ -15,38 +15,48 @@ module VagrantPlugins
           
           ssh_info = machine.ssh_info
           
-          # Create the guest path
+          # Create empty guestpath
+          machine.communicate.sudo("rm -rf '#{guestpath}'")
           machine.communicate.sudo("mkdir -p '#{guestpath}'")
           machine.communicate.sudo("chown #{ssh_info[:username]} '#{guestpath}'")
 
-          #copy up everything at the beginning
           Net::SCP.start(ssh_info[:host], ssh_info[:username], 
                         { :port => ssh_info[:port], 
-                          :keys => [ ssh_info[:private_key_path],
-                          :paranoid => false ] }) do |scp|
-            scp.upload! hostpath, guestpath, :recursive => true 
-          end
+                          :keys => [ ssh_info[:private_key_path] ],
+                          :paranoid => false }) do |scp|
 
-          @env.ui.info "Watching #{hostpath} for changes..."
-
-          Listen.to(hostpath) do |modified, added, removed|
-            Net::SCP.start(ssh_info[:host], ssh_info[:username], 
-                        { :port => ssh_info[:port], 
-                          :keys => [ ssh_info[:private_key_path],
-                          :paranoid => false ] }) do |scp|
-              (modified_list << added_list).flatten.each do |file|
-                remote_file = file.gsub(hostpath, guestpath)
-                @env.ui.info "Uploading #{file} to #{remote_file}"
-                scp.upload! file, remote_file
+            #copy up everything at the beginning
+            @env.ui.info "Uploading {host}::#{hostpath} to {guest}::#{guestpath}"
+            Dir.glob("#{hostpath}**/*", File::FNM_DOTMATCH).each do |file|
+              remote_file = file.gsub(hostpath, guestpath)
+              if File.stat(file).file?  
+                scp.upload!( file, remote_file ) do |ch, name, sent, total|
+                  @env.ui.info "\r#{name}: #{(sent.to_f * 100 / total.to_f).to_i}%"
+                end
               end
-              removed.each do |file|
-                remote_file = file.gsub(hostpath, guestpath)
-                @env.ui.info "Deleting #{remote_file}"
-                machine.communicate.sudo("rm #{remote_file}")
+              if File.directory?(file)
+                machine.communicate.sudo("mkdir -p '#{remote_file}'")
+                machine.communicate.sudo("chown #{ssh_info[:username]} '#{remote_file}'")
               end
             end
 
-          end
+            @env.ui.info "Watching {host}::#{hostpath} for changes..."
+
+            Listen.to(hostpath) do |modified, added, removed|
+              (modified << added).flatten.each do |file|
+                remote_file = file.gsub(hostpath, guestpath)
+                @env.ui.info "Uploading {host}::#{file} to {guest VM}::#{remote_file}"
+                scp.upload!( file, remote_file ) do |ch, name, sent, total|
+                  @env.ui.info "\r#{name}: #{(sent.to_f * 100 / total.to_f).to_i}%"
+                end
+              end
+              removed.each do |file|
+                remote_file = file.gsub(hostpath, guestpath)
+                @env.ui.info "Deleting {guest VM}::#{remote_file}"
+                machine.communicate.sudo("rm #{remote_file}")
+              end
+            end # Listen
+          end # Net::SCP.start
 
         end
 
@@ -57,41 +67,11 @@ module VagrantPlugins
           hostpath  = File.expand_path(machine.config.sync.host_folder, @env.root_path)
           guestpath = machine.config.sync.guest_folder
 
-          # Make sure there is a trailing slash on the host path to
-          # avoid creating an additional directory with rsync
+          # Make sure there is a trailing slash both paths
           hostpath = "#{hostpath}/" if hostpath !~ /\/$/
+          guestpath = "#{guestpath}/" if guestpath !~ /\/$/
 
           [hostpath, guestpath]
-      end
-
-      def trigger_unison_sync(machine)
-        hostpath, guestpath = init_paths machine
-
-        @env.ui.info "Unisoning changes from {host}::#{hostpath} --> {guest VM}::#{guestpath}"
-
-        ssh_info = machine.ssh_info
-
-        # Create the guest path
-        machine.communicate.sudo("mkdir -p '#{guestpath}'")
-        machine.communicate.sudo("chown #{ssh_info[:username]} '#{guestpath}'")
-
-        # Unison over to the guest path using the SSH info
-        command = [
-          "unison", "-batch",
-          "-ignore=Name {.git*,.vagrant/,*.DS_Store}",
-          "-sshargs", "-p #{ssh_info[:port]} -o StrictHostKeyChecking=no -i #{ssh_info[:private_key_path]}",
-          hostpath,
-          "ssh://#{ssh_info[:username]}@#{ssh_info[:host]}/#{guestpath}"
-         ]
-
-        r = Vagrant::Util::Subprocess.execute(*command)
-        if r.exit_code != 0
-          raise Vagrant::Errors::UnisonError,
-            :command => command.inspect,
-            :guestpath => guestpath,
-            :hostpath => hostpath,
-            :stderr => r.stderr
-        end
       end
      
     end
